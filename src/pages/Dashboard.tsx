c09686c77b4175d8a5d593e27f0a6e1a974007d0
Akhilesh Mishra <akhileshmish300@yahoo.in>
2026-03-23 22:28:57 +0530

    added calendar and types

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,10 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react"
 import { Appbar } from "../components/Appbar"
-import axios from "axios";
+import axios from "axios"
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
+import "../datepicker-custom.css"
+import { ChevronDown } from 'lucide-react'
 
 interface Item {
     id: number
@@ -11,14 +15,42 @@ interface Item {
 export const Dashboard = () => {
     const [itemName, setItemName] = useState("")
     const [amount, setAmount] = useState("")
+    const [amountType, setAmountType] = useState("Expenses")
     const [items, setItems] = useState<Item[]>([])
     const [loading, setLoading] = useState(true)
     const [buttonText, setButtonText] = useState(false)
     const [selectedItems, setSelectedItems] = useState<number[]>([])
     const [editId, setEditId] = useState<number>(0)
     const [tempName, setTempName] = useState<string>("")
-    const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-    const [selectedExpense, setSelectedExpense] = useState<Item | null>(null);
+    const [isEditModalOpen, setIsEditModalOpen] = useState(false)
+    const [selectedExpense, setSelectedExpense] = useState<Item | null>(null)
+    const [activeTab, setActiveTab] = useState("Expenses")
+    const [selectedDate, setSelectedDate] = useState(new Date())
+    const [showPicker, setShowPicker] = useState(false)
+    const [isOpen, setIsOpen] = useState(false)
+    const dropdownRef = useRef<HTMLDivElement>(null)
+    const types = ["Savings", "Expenses", "Investments"]
+
+    useEffect(() => {
+        const fetchMonthlyData = async () => {
+            const month = selectedDate.getMonth() + 1
+            const year = selectedDate.getFullYear()
+            const type = activeTab.toLowerCase()
+
+            try {
+                const response = await axios.get("http://localhost:3000/api/v1/account/monthly-summary", {
+                    params: { month, year, type },
+                    headers: {
+                        Authorization: localStorage.getItem("token")
+                    }
+                })
+                setItems(response.data || [])
+            } catch (err) {
+                console.error("Failed to fetch data for selected month", err)
+            }
+        }
+        fetchMonthlyData()
+    }, [selectedDate])
 
     const toggleItemSelection = (id: number) => {
         setSelectedItems((prev) =>
@@ -31,9 +63,9 @@ export const Dashboard = () => {
         setSelectedItems([])
     }
 
-    const fetchExpenses = useCallback(async () => {
+    const fetchExpenses = useCallback(async (type = "expenses") => {
         try {
-            const response = await axios.get("https://api.expensetracker24.in/api/v1/account/items", {
+            const response = await axios.get(`http://localhost:3000/api/v1/account/items/${type}`, {
                 headers: {
                     Authorization: localStorage.getItem("token")
                 }
@@ -53,11 +85,13 @@ export const Dashboard = () => {
     const addItem = async () => {
         if (!itemName || !amount) return
         setLoading(true)
+        setActiveTab(amountType)
         
         try {
-            await axios.post("https://api.expensetracker24.in/api/v1/account/additem", {
+            await axios.post("http://localhost:3000/api/v1/account/additem", {
                 item: itemName,
                 cost: parseInt(amount),
+                type: amountType.toLowerCase(),
                 userId: localStorage.getItem("email")
             }, {
                 headers: {
@@ -65,7 +99,7 @@ export const Dashboard = () => {
                 }
             })
 
-            await fetchExpenses()
+            await fetchExpenses(amountType.toLowerCase())
             setItemName("")
             setAmount("")
         } catch (err) {
@@ -84,7 +118,7 @@ export const Dashboard = () => {
 
         try {
             const deleteRequests = selectedItems.map((id) => 
-                axios.delete(`https://api.expensetracker24.in/api/v1/account/removeitem/user/${userId}/items/${id}`, {
+                axios.delete(`http://localhost:3000/api/v1/account/removeitem/user/${userId}/items/${id}`, {
                     headers: {
                         Authorization: localStorage.getItem("token")
                     }
@@ -93,7 +127,7 @@ export const Dashboard = () => {
 
             await Promise.all(deleteRequests)
 
-            await fetchExpenses()
+            await fetchExpenses(activeTab.toLowerCase())
             setSelectedItems([])
             setLoading(false)
             setButtonText(false)
@@ -124,7 +158,7 @@ export const Dashboard = () => {
             const token = localStorage.getItem("token")
             const userEmail = localStorage.getItem("email")
 
-            await axios.put("https://api.expensetracker24.in/api/v1/account/changeitem", {
+            await axios.put("http://localhost:3000/api/v1/account/changeitem", {
                 newItemName: itemName,
                 cost: Number(amount),
                 email: userEmail,
@@ -137,7 +171,7 @@ export const Dashboard = () => {
                 }
             });
 
-            await fetchExpenses()
+            await fetchExpenses(activeTab.toLowerCase())
             setEditId(0)
             setTempName("")
             setIsEditModalOpen(false)
@@ -151,6 +185,28 @@ export const Dashboard = () => {
         }
     }
 
+    
+    const tabs = ["Savings", "Expenses", "Investments"]
+
+    const handleTabClick = (tab: string) => {
+        setActiveTab(tab);
+        const time = new Date()
+        if (time.getMonth() === selectedDate.getMonth()) {
+            console.log(time.getMonth(), selectedDate.getMonth())
+            fetchExpenses(tab.toLowerCase())
+        }
+    }
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+                setIsOpen(false);
+            }
+        }
+        document.addEventListener("mousedown", handleClickOutside)
+        return () => document.removeEventListener("mousedown", handleClickOutside)
+    }, [])
+
     return (
         <div className="min-h-screen bg-slate-50">
             <Appbar />
@@ -160,6 +216,32 @@ export const Dashboard = () => {
                 <div className="rounded-2xl bg-white w-full max-w-lg p-8 h-max shadow-lg border border-gray-100">
                     <h2 className="text-xl font-bold mb-6 text-gray-800">Add New Expense</h2>
                     <div className="space-y-4 text-left">
+                        <div className="relative" ref={dropdownRef}>
+                            <label className="text-sm font-semibold text-gray-500 ml-1">AMOUNT TYPE</label>
+                            <button
+                                onClick={() => setIsOpen(!isOpen)}
+                                className="w-full flex items-center justify-between bg-white border border-gray-200 rounded-xl px-4 py-4 transition-all focus:ring-2 focus:ring-blue-500 outline-none"
+                            >
+                                {amountType}
+                                <ChevronDown className={`w-4 h-4 transition-transform ${isOpen ? 'rotate-180' : ''}`} />
+                            </button>
+                            {isOpen && (
+                                <div className="absolute z-50 w-full mt-2 bg-white border border-gray-100 shadow-xl rounded-xl overflow-hidden animate-in fade-in zoom-in duration-200">
+                                    {types.map((type) => (
+                                        <div
+                                            key={type}
+                                            onClick={() => {
+                                                setAmountType(type);
+                                                setIsOpen(false);
+                                            }}
+                                            className="px-4 py-3 text-sm hover:bg-blue-50 hover:text-blue-600 cursor-pointer transition-colors"
+                                            >
+                                            {type}
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
+                        </div>
                         <div>
                             <label className="text-sm font-semibold text-gray-500 ml-1">ITEM NAME</label>
                             <input
@@ -205,8 +287,34 @@ export const Dashboard = () => {
 
                 {/* Right Card: Summary List */}
                 <div className="rounded-2xl bg-white w-full max-w-lg p-8 min-h-[450px] shadow-lg border border-gray-100 flex flex-col">
-                    <div className="flex flex-col md:flex-row md:justify-between mb-6 border-b pb-2">
-                        <h2 className="text-xl font-bold text-gray-800">History</h2>
+                    <div className="flex flex-col md:flex-row md:justify-between mb-3 border-b pb-2">
+                        <div className="flex justify-between">
+                            <h2 className="text-xl font-bold text-gray-800">History</h2>
+                            <div className="relative">
+                                <button className="pl-4 text-gray-400 hover:text-blue-700 pt-1" onClick={() => setShowPicker(!showPicker)}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+                                    </svg>
+                                </button>
+                                {showPicker && (
+                                    <div className="absolute right-100 z-[100] mt-2 shadow-xl border rounded-lg overflow-hidden">
+                                        <DatePicker
+                                            selected={selectedDate}
+                                            onChange={(date: Date | null) => {
+                                                if (date) {
+                                                    setSelectedDate(date)
+                                                    setShowPicker(false)
+                                                }
+                                            }}
+                                            dateFormat="MM/yyyy"
+                                            showMonthYearPicker
+                                            inline
+                                        />
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+
                         
                         <div className="md:flex-row md:justify-end items-center mb-6 mt-4 md:mt-0">
                             <button onClick={handleDeleteRequest} disabled={!selectedItems.length || loading} className={`text-white bg-red-400 px-3 py-1 rounded-full text-sm font-semibold shadow-blue-200 shadow-md mr-4
@@ -234,6 +342,33 @@ export const Dashboard = () => {
                             <span className="bg-blue-100 text-blue-600 px-3 py-1 rounded-full text-xs font-bold">
                                 {items.length === 0 ? `NO ITEMS` : items.length === 1 ? `1 ITEM` : `${items.length} ITEMS`}
                             </span>
+                        </div>
+                    </div>
+
+                    <div className="w-full max-w-md mx-auto">
+                        {/* Container for Tabs */}
+                        <div className="relative flex justify-around mb-4 border-b border-gray-100 pb-2">
+                            
+                            {tabs.map((tab) => (
+                                <div
+                                    key={tab}
+                                    onClick={() => handleTabClick(tab)}
+                                    className={`flex-1 text-center text-sm font-semibold cursor-pointer transition-colors duration-300 ${
+                                    activeTab === tab ? "text-blue-600" : "text-gray-400 hover:text-gray-700"
+                                    }`}
+                                >
+                                    {tab}
+                                </div>
+                            ))}
+
+                            {/* The Animated Underline */}
+                            <div 
+                                className="absolute bottom-0 h-0.5 bg-blue-600 transition-all duration-300 ease-in-out"
+                                style={{ 
+                                    width: "90px", 
+                                    left: `calc(${(tabs.indexOf(activeTab) * 100 / tabs.length) + (100 / tabs.length / 2)}% - 45px)` 
+                                }}
+                            />
                         </div>
                     </div>
                     
@@ -338,11 +473,3 @@ export const Dashboard = () => {
         </div>
     );
 }
-
-
-
-
-
-
-
-;
